ccc96a410fc0980131e31ebc0ed21ef0f5b81a54
njredmitch <njredmitch12@gmail.com>
2023-06-18 15:12:31 -0400

    added week interface and class impl

---
+++ src/main/java/cs3500/pa05/model/IWeek.java
@@ -0,0 +1,27 @@
+package cs3500.pa05.model;
+
+import java.util.List;
+import javafx.event.Event;
+
+public interface IWeek {
+  /**
+   * retrieves tasks of a week
+   *
+   * @return - list of tasks
+   */
+  List<Task> getTasks();
+
+  /**
+   * retrieves events of a week
+   *
+   * @return - list of events taking place
+   */
+  List<Event> getEvents();
+
+  /**
+   * retrieves the weeks name
+   *
+   * @return - the name
+   */
+  String getName();
+}
