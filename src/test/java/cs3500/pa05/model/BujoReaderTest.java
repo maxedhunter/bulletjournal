b71c26ec21138c7ab4f5e4e1f6411fd9c2265920
maxedhunter <maxwelljhe@gmail.com>
2023-06-21 17:47:50 -0400

    Updated BujoReader testing.

--- src/test/java/cs3500/pa05/model/BujoReaderTest.java
+++ src/test/java/cs3500/pa05/model/BujoReaderTest.java
@@ -1,6 +1,7 @@
 package cs3500.pa05.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,9 @@ import org.junit.jupiter.api.Test;
 class BujoReaderTest {
   private static final String PATH = "src/test/resources/output.bujo";
 
+  /**
+   * Tests reading from a file.
+   */
   @Test
   void testRead() {
     BujoReader reader = new BujoReader(PATH);
@@ -21,9 +25,34 @@ class BujoReaderTest {
     List<Task> tasks = week.getTasks();
     Task task = tasks.get(0);
     assertEquals("todo", task.getName());
+    assertEquals("do it!", task.getDescription());
+    assertEquals(DayEnum.MONDAY, task.getDay());
+    assertEquals(false, task.getCompletion());
+
+    List<Event> events = week.getEvents();
+    Event event = events.get(0);
+    assertEquals("an event", event.getName());
+    assertEquals("test event", event.getDescription());
+    assertEquals(DayEnum.MONDAY, event.getDay());
+
+    Time time = event.getStartTime();
+    assertEquals(5, time.getHour());
+    assertEquals(6, time.getMinute());
+
+    assertEquals(7, event.getDuration());
+
+    Day monday = week.getDays().get(DayEnum.MONDAY);
+    assertEquals(1, monday.getTasks().size());
+    assertEquals(1, monday.getEvents().size());
   }
 
+  /**
+   * Tests exception
+   */
   @Test
-  void testParseWeek() {
+  void testException() {
+    BujoReader reader = new BujoReader("thisFilesDoesntExist");
+    assertThrows(RuntimeException.class,
+        () -> reader.read());
   }
 }
